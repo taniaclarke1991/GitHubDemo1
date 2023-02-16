8057aaff65ba9812cb78f23421ac9d02a45e7bad
unknown <taniaclarke1991@gmail.com>
2023-02-16 15:11:59 +0530

    TC first commit Demo1

---
+++ src/main/java/dataDriven/GitHubDemo1/HomePage.java
@@ -0,0 +1,10 @@
+package dataDriven.GitHubDemo1;
+
+public class HomePage {
+
+	
+	public void HP()
+	{
+		System.out.println("Home Page");
+	}
+}
---
+++ src/test/java/dataDriven/GitHubDemo1/HomePageTest.java
@@ -0,0 +1,9 @@
+package dataDriven.GitHubDemo1;
+
+public class HomePageTest {
+
+	public void HPT()
+	{
+		System.out.println("Home Page Test");
+	}
+}
